72e652ee83c11f371f2c402373b155b2a44fb491
Jason Lau <jason_lau@live.com>
2014-03-06 17:47:01 -0600

    Fixed a bug in the profile short code.
    Fixed a bug in the profile short code.

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Contributors: Jason Lau
 Donate link: http://jasonlau.biz
 Tags: user, users, meta, usermeta, wp_usermeta, data, table, database, edit, add, update, delete, save, saved, list, manage, manager, management, customize, custom, create, activate, register, registers, short, tag, short code, member, members, admin, administrate, administrator, administrative, tool, plugin, module, addon, jason, lau, jasonlau, jasonlau.biz, profile, field, fields, extra
 Requires at least: 3.3.1
-Stable tag: 3.2.4
+Stable tag: 3.2.5
 Tested up to: 3.8.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
@@ -66,6 +66,9 @@ Yes I do. Check my website at JasonLau.biz.
 1. screenshot-1.png The User Meta Manager Home screen.
 
 == Changelog ==
+
+= 3.2.5 =
+Fixed a bug in the profile short code.
 
 = 3.2.4 =
 Fixed a bug.
@@ -482,3 +485,6 @@ Arrays are now properly displayed on the Home screen.
 
 = 3.2.4 =
 Fixed a bug.
+
+= 3.2.5 =
+Fixed a bug in the profile short code.
--- user-meta-manager.php
+++ user-meta-manager.php
@@ -4,7 +4,7 @@
  * Plugin Name: User Meta Manager
  * Plugin URI: https://github.com/jasonlau/Wordpress-User-Meta-Manager
  * Description: Add, edit, or delete user meta data with this handy plugin. Easily restrict access or insert user meta data into posts or pages and more. <strong>Get the Pro extension <a href="http://jasonlau.biz/home/membership-options#umm-pro">here</a>.</strong>
- * Version: 3.2.4
+ * Version: 3.2.5
  * Author: Jason Lau
  * Author URI: http://jasonlau.biz
  * Text Domain: user-meta-manager
@@ -31,7 +31,7 @@
     exit('Please don\'t access this file directly.');
 }
 
-define('UMM_VERSION', '3.2.4');
+define('UMM_VERSION', '3.2.5');
 define("UMM_PATH", plugin_dir_path(__FILE__) . '/');
 define("UMM_SLUG", "user-meta-manager");
 define("UMM_AJAX", "admin-ajax.php?action=umm_switch_action&amp;umm_sub_action=");
@@ -1697,6 +1697,7 @@ function umm_shortcode($atts, $content){
             });
             $("div.umm-userprofile-loading").fadeOut("slow");
             $("div.umm-userprofile").fadeIn("slow");
+            $(".hidden").hide();
        };
        fetch_profile("div.umm-userprofile", "' . $profile_link . '", "form#your-profile", activate_form);
 	});
